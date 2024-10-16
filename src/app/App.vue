e62d9a38cc4797de5acb059a765d2e2a10ead571
UltraPokemosha <sfygithub@gmail.com>
2024-10-16 22:05:18 +0300

    feat: добавил шрифты, без scss переменных

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { RouterView } from "vue-router";
+import AppLoader from "./AppLoader.vue";
+
+const isLoading = ref(true);
+
+setTimeout(() => (isLoading.value = false), Math.random() * 2500);
 </script>
 
 <template>
   <RouterView />
+  <AppLoader v-if="isLoading" />
 </template>
 
 <style src="./styles.scss" lang="scss"></style>
