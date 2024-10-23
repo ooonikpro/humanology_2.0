c9f2b4c2b5b25858488d9c104d86ee4dce4e90ee
Никита Овечкин <nikita.ovechkin@gazprombank.ru>
2024-10-23 14:53:30 +0200

    chore: удален пример из главной страницы

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,3 @@
-<script setup lang="ts">
-import { ref } from "vue";
-import { UiGenderSwitch } from "@shared/ui";
-import { GenderEnum } from "@shared/constants/gender";
-
-const gender = ref(GenderEnum.male);
-</script>
-
 <template>
-  <div>
-    Home
-    <UiGenderSwitch v-model="gender" />
-  </div>
+  <div>Home</div>
 </template>
