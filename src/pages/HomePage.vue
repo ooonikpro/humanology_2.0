6e28ddd4896e063ae90c9760bf8c651ed1cef34b
ninja095 <ninja095@gmail.com>
2024-10-24 13:14:46 +0200

    UiRowDual добавлен

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,3 +1,7 @@
 <template>
   <div>Home</div>
+  <UiRowDual leftText="Статик" rightText="Динамик" />
 </template>
+<script setup>
+import UiRowDual from "@shared/ui/UiRowDual.vue";
+</script>
---
+++ src/shared/ui/UiRowDual.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+const props = defineProps<{
+  leftText: string;
+  rightText: string;
+}>();
+</script>
+
+<template>
+  <div class="ui-row-dual">
+    <span class="ui-row-dual__text">{{ leftText }}</span>
+    <span class="ui-row-dual__line"></span>
+    <span class="ui-row-dual__text">{{ rightText }}</span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "../styles/variables/colors";
+.ui-row-dual {
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+  width: 100%;
+  height: 32px;
+}
+
+.ui-row-dual__text {
+  font-size: 18px;
+  text-decoration: none;
+  padding-bottom: 2px;
+}
+
+.ui-row-dual__line {
+  flex-grow: 1;
+  border-bottom: 1px dotted black;
+  margin: 0 10px;
+}
+</style>
