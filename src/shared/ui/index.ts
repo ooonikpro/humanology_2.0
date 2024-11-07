1c1f22f14582e8757aaa127730949d13a19169fe
UltraPokemosha <sfygithub@gmail.com>
2024-11-08 01:18:36 +0300

    issue-20: refactor: удалил ненужный компонент UiRandomImg

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,4 +2,3 @@ export { default as UiGenderSwitcher } from "./UiGenderSwitcher.vue";
 export { default as UiSvg } from "./UiSvg.vue";
 export { default as UiRowDual } from "./UiRowDual.vue";
 export { default as UiText } from "./UiText.vue";
-export { default as UiRandomImg } from "./UiRandomImg.vue";
