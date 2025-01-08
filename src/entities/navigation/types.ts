9d5040ecca5f724c3e2c2d8d2ef7b7bdc217e840
UltraPokemosha <sfygithub@gmail.com>
2025-01-08 22:20:22 +0300

    refactor: заменил кастомный тип типом из коробки (ToCallback)

--- src/entities/navigation/types.ts
+++ src/entities/navigation/types.ts
@@ -1,8 +1,8 @@
-import type { RegisteredRouter, ResolvedRoute, Url } from "@kitbag/router";
+import type { ToCallback } from "@kitbag/router";
 import type { IconNameType } from "@types";
 
 export type NavigationTabPropsType = {
-  to: (resolve: RegisteredRouter["resolve"]) => ResolvedRoute | Url | undefined;
+  to: ToCallback;
   iconName: IconNameType;
   label: string;
 };
