2a2ca615ae26be4f0d94189ebdc4190adabfe3f1
Никита Овечкин <nikita.ovechkin@gazprombank.ru>
2025-03-03 16:53:35 +0200

    feat: добавлены темные оттенки цветов социотипов

--- src/entities/sociotypes/lib/createColorQuadraStyle.ts
+++ src/entities/sociotypes/lib/createColorQuadraStyle.ts
@@ -3,6 +3,7 @@ import type { QuadrasType } from "@types";
 const createColorQuadraStyle = (quadra: QuadrasType) => {
   return {
     "--color-quadra": `var(--color-quadra-${quadra})`,
+    "--color-quadra-dark": `var(--color-quadra-${quadra}-dark)`,
   };
 };
 
--- src/entities/sociotypes/lib/createColorRoleStyle.ts
+++ src/entities/sociotypes/lib/createColorRoleStyle.ts
@@ -5,6 +5,7 @@ import getRoleQuadra from "../model/methods/getRoleQuadra";
 const createColorRoleStyle = (role: RoleType) => {
   return {
     "--color-role": `var(--color-quadra-${getRoleQuadra(role)})`,
+    "--color-role-dark": `var(--color-quadra-${getRoleQuadra(role)}-dark)`,
   };
 };
 
