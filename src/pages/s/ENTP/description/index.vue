bc7692d11ae95a94890651f38630c87d4a6e7512
älёsha <konight@gmail.com>
2025-04-14 00:46:21 +0300

    ENTP description

--- src/pages/s/ENTP/description/index.vue
+++ src/pages/s/ENTP/description/index.vue
@@ -1,12 +1,3 @@
-<script lang="ts" setup>
-import {
-  SociotypePortraitSwitcher,
-  SociotypeBodySwitcher,
-  SociotypeFaceSwitcher,
-  SociotypeProvider,
-} from "@entities/sociotypes";
-</script>
-
 <template>
   <div class="text">
     <section>
@@ -339,11 +330,5 @@ import {
     padding-bottom: 16px;
     border-bottom: solid 1px colors.$grey;
   }
-}
-
-.bodies {
-  width: 100%;
-  margin-top: 16px;
-  margin-bottom: 16px;
 }
 </style>
