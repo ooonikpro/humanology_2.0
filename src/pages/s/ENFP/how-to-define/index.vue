8031683ea1683ccc3b4aa1d84ce2069a01fa622a
älёsha <konight@gmail.com>
2025-07-30 02:25:41 +0300

    Content update

--- src/pages/s/ENFP/how-to-define/index.vue
+++ src/pages/s/ENFP/how-to-define/index.vue
@@ -1,11 +1,73 @@
 <script lang="ts" setup>
-
+import {
+  SociotypePortraitSwitcher,
+  SociotypeBodySwitcher,
+  SociotypeFaceSwitcher,
+  SociotypeProvider,
+} from "@entities/sociotypes";
 </script>
 
 <template>
+  <div class="text g-page">
+    <SociotypeProvider id="ENFP" force-tag="section">
+      <UiText preset="large" color="role">Портреты</UiText>
+      <SociotypePortraitSwitcher id="ENFP" />
+    </SociotypeProvider>
 
+    <section>
+      <UiText preset="large" color="role">Телосложение</UiText>
+      <UiText>
+        Телосложение астеническое, рост часто выше среднего, нередко выражена
+        сутулость в грудном отделе позвоночника.
+      </UiText>
+      <SociotypeBodySwitcher id="ENFP" />
+    </section>
+
+    <section>
+      <UiText preset="large" color="role">Форма и черты лица</UiText>
+
+      <UiText>
+        Овальная или треугольная. Черты — немного закругленные, «мягкие», без
+        резкой выраженности отдельных элементов.
+      </UiText>
+
+      <SociotypeFaceSwitcher id="ENFP" />
+    </section>
+
+    <section>
+      <UiText preset="large" color="role">Морщины</UiText>
+      <UiText>
+        Сильно выражены вертикальные, расположенные на межбровном расстоянии.
+      </UiText>
+    </section>
+
+    <section>
+      <UiText preset="large" color="role">Взгляд</UiText>
+      <UiText>
+        Очень цепкий, хорошо сфокусированный, внимательный, прежде всего — к
+        людям, «психологичный», немного «грустный», понимающий, весьма
+        располагающий к общению с его обладателем.
+      </UiText>
+    </section>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+@use "@shared/styles/variables/colors";
 
+.text {
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    padding-bottom: 16px;
+    border-bottom: solid 1px colors.$grey;
+  }
+}
+
+.bodies {
+  width: 100%;
+  margin-top: 16px;
+  margin-bottom: 16px;
+}
 </style>
--- src/pages/s/ESTJ/how-to-define/index.vue
+++ src/pages/s/ESTJ/how-to-define/index.vue
@@ -1,11 +1,77 @@
 <script lang="ts" setup>
-
+import {
+  SociotypePortraitSwitcher,
+  SociotypeBodySwitcher,
+  SociotypeFaceSwitcher,
+  SociotypeProvider,
+} from "@entities/sociotypes";
 </script>
 
 <template>
+  <div class="text g-page">
+    <SociotypeProvider id="ESTJ" force-tag="section">
+      <UiText preset="large" color="role">Портреты</UiText>
+      <SociotypePortraitSwitcher id="ESTJ" />
+    </SociotypeProvider>
 
+    <section>
+      <UiText preset="large" color="role">Телосложение</UiText>
+      <UiText>
+        Стеническое «сухое», без склонности к полноте. Фигура часто спортивная,
+        с хорошей осанкой и «военной» выправкой, но не такая массивная и
+        коренастая, как у Лидера.
+      </UiText>
+      <SociotypeBodySwitcher id="ESTJ" />
+    </section>
+
+    <section>
+      <UiText preset="large" color="role">Форма и черты лица</UiText>
+
+      <UiText>
+        Прямоугольная, реже — с треугольной нижней частью. Черты — чёткие,
+        резкие, рельефные, «правильные» или «плакатные», отмечается жесткий
+        «волевой» рот.
+      </UiText>
+
+      <SociotypeFaceSwitcher id="ESTJ" />
+    </section>
+
+    <section>
+      <UiText preset="large" color="role">Морщины</UiText>
+      <UiText>
+        Слабо выраженные динамические, на фоне которых присутствуют и
+        вертикальные статические складки на переносице. Присутствуют также
+        «эндемичные» морщины: расходящиеся от глаз к вискам, они символизируют
+        пристальное внимание к мелочам, деталям.
+      </UiText>
+    </section>
+
+    <section>
+      <UiText preset="large" color="role">Взгляд</UiText>
+      <UiText>
+        Открытый, спокойный, холодно-приветливый, без дружелюбия, но и без
+        агрессии, внимательный к окружающему миру и происходящим в нём явлениям.
+      </UiText>
+    </section>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+@use "@shared/styles/variables/colors";
 
+.text {
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    padding-bottom: 16px;
+    border-bottom: solid 1px colors.$grey;
+  }
+}
+
+.bodies {
+  width: 100%;
+  margin-top: 16px;
+  margin-bottom: 16px;
+}
 </style>
